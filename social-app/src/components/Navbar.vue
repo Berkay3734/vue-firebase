ae2d70056dd7b82170dc26dc63116816ae0d73b1
Berkay3734 <berkay.gameofbilisim.com>
2023-03-04 00:11:46 +0300

    .

---
+++ social-app/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Sosyal</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        </ul>
+        <div class="d-flex">
+            <button class="btn btn-outline-success" type="submit">Giriş</button>
+            <button class="btn btn-outline-danger" type="submit">Çıkış</button>
+        </div>
+        </div>
+    </div>
+    </nav>
+</template>
